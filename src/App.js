c56098a1079cd6a6e1a08c5a2f4a0dfc759742b7
dilan <dilan@cleato.com>
2021-10-04 04:21:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useEffect } from "react";
+import axios from "axios";
+import { useExportData } from "react-table-plugins";
+import Table from "./Table";
+import "./App.css";
+
+const API_HOST = "http://localhost:8000";
+
+const REPORT_API_URL = `${API_HOST}/api/reports/turnover`;
 
 function App() {
+  let columns = useMemo(
+    () => [
+      {
+        Header: "Report - 7 Days Turnover",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "name",
+          },
+          {
+            Header: "Week End Date",
+            accessor: "week_end_date",
+          },
+          {
+            Header: "Turnover (EURO)",
+            accessor: "turnover",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  const [data, setBody] = useState([]);
+
+  const fetchTurnOverReport = (type) => {
+    (async () => {
+      const result = await axios.post(`${REPORT_API_URL}`, { type });
+      setBody(result.data.body);
+
+      //  setColumns({ columns: result.data.columns});
+      //columns=[columns,...{coloums:result.data.columns}];
+      //setBody({ body: result.data.body});
+    })();
+  };
+
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    fetchTurnOverReport(value);
+  };
+
+  useEffect(() => {
+    fetchTurnOverReport("brand");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <select onChange={handleFilterChange} >
+        <option value="brand">Brand</option>
+        <option value="day">Day</option>
+      </select>
+      </div>
+      <div>
+      <Table columns={columns} data={data} />
+      </div>
     </div>
   );
 }
